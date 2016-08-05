279026c2d28e34f414815b917ce012fea66e8d51
Adam Gall <galladamj@gmail.com>
2016-08-04 23:33:06 -0400

    Hook up Export button and implement Number of Attendees label on MainViewController

--- Attendance/MainViewController.m
+++ Attendance/MainViewController.m
@@ -6,22 +6,36 @@
 //  Copyright © 2016 gallio. All rights reserved.
 //
 
+#import "AppDelegate.h"
+#import "Attendee.h"
 #import "MainViewController.h"
 
 @interface MainViewController ()
+
+@property (nonatomic, strong) IBOutlet UIButton *export;
+@property (nonatomic, strong) IBOutlet UILabel *savedAttendees;
+
+@property (nonatomic, strong) NSArray *allAttendees;
 
 @end
 
 @implementation MainViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self refreshAttendees];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)refreshAttendees {
+    self.allAttendees = [self fetchAllAttendees];
+    self.savedAttendees.text = [NSString stringWithFormat:@"%ld Saved Attendees", (long)self.allAttendees.count];
+}
+
+- (NSArray *)fetchAllAttendees {
+    AppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+    NSManagedObjectContext *managedObjectContext = [appDelegate managedObjectContext];
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Attendee"];
+    return [managedObjectContext executeFetchRequest:request error:nil];
 }
 
 @end

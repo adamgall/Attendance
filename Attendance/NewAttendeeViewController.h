e60585a5b17f0247c206af0fb9944928ef34f0a3
Adam Gall <galladamj@gmail.com>
2016-08-04 23:36:56 -0400

    Create NewAttendeeViewController, some properties, and hook them up to the storyboard

---
+++ Attendance/NewAttendeeViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewAttendeeViewController.h
+//  Attendance
+//
+//  Created by Adam Gall on 8/4/16.
+//  Copyright © 2016 gallio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewAttendeeViewController : UIViewController
+
+@end
---
+++ Attendance/NewAttendeeViewController.m
@@ -0,0 +1,23 @@
+//
+//  NewAttendeeViewController.m
+//  Attendance
+//
+//  Created by Adam Gall on 8/4/16.
+//  Copyright © 2016 gallio. All rights reserved.
+//
+
+#import "NewAttendeeViewController.h"
+
+@interface NewAttendeeViewController ()
+
+@property (nonatomic, strong) IBOutlet UITextField *name;
+@property (nonatomic, strong) IBOutlet UITextField *email;
+@property (nonatomic, strong) IBOutlet UITextField *phone;
+@property (nonatomic, strong) IBOutlet UIButton *submit;
+@property (nonatomic, strong) IBOutlet UILabel *error;
+
+@end
+
+@implementation NewAttendeeViewController
+
+@end

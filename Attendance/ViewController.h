f4b11e28072b464d9b53a862499d1673054c789b
Adam Gall <galladamj@gmail.com>
2016-08-04 23:16:46 -0400

    Initial Commit

---
+++ Attendance/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Attendance
+//
+//  Created by Adam Gall on 8/4/16.
+//  Copyright © 2016 gallio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Attendance/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Attendance
+//
+//  Created by Adam Gall on 8/4/16.
+//  Copyright © 2016 gallio. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

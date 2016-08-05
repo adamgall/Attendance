f407c22dc6aef47d0065253928b48ab3bac8af15
Adam Gall <galladamj@gmail.com>
2016-08-05 00:10:14 -0400

    Save a new Attendee object to Core Data when tapping the Submit button

--- Attendance/NewAttendeeViewController.m
+++ Attendance/NewAttendeeViewController.m
@@ -6,6 +6,8 @@
 //  Copyright © 2016 gallio. All rights reserved.
 //
 
+#import "AppDelegate.h"
+#import "Attendee.h"
 #import "NewAttendeeViewController.h"
 
 @interface NewAttendeeViewController ()
@@ -19,5 +21,51 @@
 @end
 
 @implementation NewAttendeeViewController
+
+#pragma mark - UIViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.error.hidden = YES;
+}
+
+#pragma mark - Touch events
+
+- (IBAction)didTapSubmit:(id)sender {
+    if ([self isValidForm]) {
+        [self saveNewAttendee];
+        [self.navigationController popViewControllerAnimated:YES];
+    }
+}
+
+#pragma mark - Private helpers
+
+- (BOOL)isValidForm {
+    BOOL isValidName = ![[self.name.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] isEqualToString:@""];
+    
+    if (!isValidName) {
+        self.error.text = @"Name cannot be blank";
+    }
+    
+    self.error.hidden = isValidName;
+    return isValidName;
+}
+
+- (void)saveNewAttendee {
+    NSString *name = [self.name.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    NSString *email = [self.email.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    NSString *phone = [self.phone.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    
+    AppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+    NSManagedObjectContext *managedObjectContext = [appDelegate managedObjectContext];
+    
+    Attendee *newAttendee = [NSEntityDescription insertNewObjectForEntityForName:@"Attendee" inManagedObjectContext:managedObjectContext];
+    
+    newAttendee.name = name;
+    newAttendee.email = email;
+    newAttendee.phone = phone;
+    
+    [managedObjectContext save:nil];
+}
 
 @end
